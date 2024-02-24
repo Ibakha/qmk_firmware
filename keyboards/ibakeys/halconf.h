1453b1d247eaf921519eee4b90e16aae3af842c9
ibakha <jacob.l.charron@gmail.com>
2024-02-24 18:38:39 -0500

    adding RBG matrix

--- keyboards/ibakeys/halconf.h
+++ keyboards/ibakeys/halconf.h
@@ -1,14 +1,14 @@
 #pragma once
 
+/** Split keyboard seriel */
 #define HAL_USE_SERIAL TRUE
+
+/** WS2812 pwm*/
+#define HAL_USE_PWM TRUE
+
+/** Bluetooth SPI*/
+#define HAL_USE_SPI TRUE
 
 #include_next <halconf.h>
 
-// #define PAL_USE_CALLBACKS TRUE
-// #define HAL_USE_SPI TRUE
-// #define SPI_USE_WAIT TRUE
-// #define SPI_SELECT_MODE SPI_SELECT_MODE_PAD
 
-// #define HAL_USE_I2C TRUE
-// #define HAL_USE_GPT TRUE
-// #define HAL_USE_DAC TRUE
--- keyboards/ibakeys/rules.mk
+++ keyboards/ibakeys/rules.mk
@@ -1,22 +1,23 @@
+# Keyboard options
 
-# Enabling split keyboard Config
+# Split Keyboard Config
 SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = usart
 
-# RGB Config
-# RGB_MATRIX_ENABLE = no
-# RBG_MATRIX_DRIVER = ws2812
-# SERIAL_DRIVER = bitbang
+# RGB Matrix Config
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
+WS2812_DRIVER = pwm
+
+# LED display Config
+OLED_ENABLE = no
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
+
+# Bluetooth Config
+BLUETOOTH_ENABLE = no
+BLUETOOTH_DRIVER = bluefruit_le
 
 # Enabling Trackball Config
 # POINTING_DEVICE_ENABLE = yes
 # POINTING_DEVICE_DRIVER = pmw3360
-
-# Enabling Bluetooth
-# BLUETOOTH_ENABLE = no
-# BLUETOOTH_DRIVER = bluefruit_le
-
-# Enabling OLED display
-# OLED_ENABLE = yes
-# OLED_DRIVER = ssd1306
-# OLED_TRANSPORT = i2c
